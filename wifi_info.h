620b5c5513d0244ff5d82ea3e9cf2756a53f77fe
Andrei <andryushik@gmail.com>
2025-12-26 15:58:41 +0100

    Refactor WiFi connection handling to use WiFiManager for improved configuration

--- wifi_info.h
+++ wifi_info.h
@@ -3,27 +3,39 @@
 
 #if defined(ESP8266)
 #include <ESP8266WiFi.h>
+#include <DNSServer.h>
+#include <ESP8266WebServer.h>
 #elif defined(ESP32)
 #include <WiFi.h>
+#include <DNSServer.h>
+#include <WebServer.h>
 #endif
 
-const char *ssid = "AI-WiFi-IoT";
-const char *password = "0657477977ai";
+#include <WiFiManager.h>
+
+#ifndef WIFI_CONFIG_PORTAL_TIMEOUT
+#define WIFI_CONFIG_PORTAL_TIMEOUT 180 // seconds
+#endif
 
 void wifi_connect()
 {
 	WiFi.persistent(false);
 	WiFi.mode(WIFI_STA);
-	WiFi.setAutoReconnect(true);
-	WiFi.begin(ssid, password);
-	Serial.println("WiFi connecting...");
-	while (!WiFi.isConnected())
+
+	WiFiManager wm;
+	wm.setWiFiAutoReconnect(true);
+	wm.setConfigPortalTimeout(WIFI_CONFIG_PORTAL_TIMEOUT);
+
+	if (!wm.autoConnect("AirHood-Setup"))
 	{
-		delay(100);
-		Serial.print(".");
+		Serial.println("WiFi config failed, restarting...");
+		delay(1000);
+#if defined(ESP8266) || defined(ESP32)
+		ESP.restart();
+#endif
 	}
-	Serial.print("\n");
+
 	Serial.printf("WiFi connected, IP: %s\n", WiFi.localIP().toString().c_str());
 }
 
-#endif /* WIFI_INFO_H_ */
+#endif
